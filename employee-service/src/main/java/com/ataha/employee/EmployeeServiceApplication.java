d5514efd8a4ac98a0ea5ae6767ddd0d469b26e92
Amine TAHA <aminetaha00@gmail.com>
2018-11-28 15:01:55 +0100

    Employee Service init commit

---
+++ employee-service/src/main/java/com/ataha/employee/EmployeeServiceApplication.java
@@ -0,0 +1,32 @@
+package com.ataha.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableDiscoveryClient
+@EnableSwagger2
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+	@Bean
+	public Docket personApi() {
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+					.apis(RequestHandlerSelectors.basePackage("com.ataha.employee.controllers"))
+					.paths(PathSelectors.any())
+				.build();
+	}
+}
